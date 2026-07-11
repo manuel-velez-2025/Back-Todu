2d927c66f7831390a4d080e5ce9b895456d970be
JORED666 <243842@ids.upchiapas.edu.mx>
2026-07-11 00:14:49 -0600

    fix: agregar search_path=users en pool de conexión

--- user-service/src/infrastructure/db/pool.ts
+++ user-service/src/infrastructure/db/pool.ts
@@ -2,6 +2,7 @@ import { Pool } from 'pg';
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  options: '-c search_path=users,public',
 });
 
 pool.on('error', (err) => {
